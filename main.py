6975fc24a9c3b1c06e31a0942442392ba537c558
LuanTruong <truongluan303@gmail.com>
2022-05-30 19:56:55 -0700

    change function name to `lambda_handler`

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 import requests
 import json
 
-#NAME OF THE FUNCTION IN AWS LAMBDA; by default, the name is "lambda_handler" or "my_handler" in the documentation
-#this must be the name of your function as defined in AWS Lambda. 
-#This will be the function that AWS Lambda calls.
-def my_serverless_function(event, context):
+# NAME OF THE FUNCTION IN AWS LAMBDA; by default, the name is "lambda_handler" or "my_handler" in the documentation
+# this must be the name of your function as defined in AWS Lambda. 
+# This will be the function that AWS Lambda calls.
+def lambda_handler(event, context):
     #IMPORTANT: json format for making the results accessible through the API Gateway
     response = {
         "statusCode": 200,
