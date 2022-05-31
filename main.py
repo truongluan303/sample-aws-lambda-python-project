5d017424cb4a8be54a5379e98420468de979371e
LuanTruong <truongluan303@gmail.com>
2022-05-30 19:55:08 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#import statements go here
+import requests
+import json
+
+#NAME OF THE FUNCTION IN AWS LAMBDA; by default, the name is "lambda_handler" or "my_handler" in the documentation
+#this must be the name of your function as defined in AWS Lambda. 
+#This will be the function that AWS Lambda calls.
+def my_serverless_function(event, context):
+    #IMPORTANT: json format for making the results accessible through the API Gateway
+    response = {
+        "statusCode": 200,
+        "body": {
+            "message": "Welcome to AWS lambda!",
+        }
+    }
+    return response
